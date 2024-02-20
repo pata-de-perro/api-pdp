9272b0a0d895a2334ee3c44439ec4a4c59b672e9
Josué Yovani <josue.yovani@outlook.com>
2024-02-19 20:48:57 -0600

    Add - Create authUser Controller

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,7 +1,7 @@
 const jsonwebtoken = require("jsonwebtoken");
 const { JWT } = require("../../config/constants");
 
-const generateToen = (payload) => {
+const generateToken = (payload) => {
   return jsonwebtoken.sign(payload, JWT.SECRET, { expiresIn: JWT.EXPIRES });
 };
 
@@ -9,4 +9,4 @@ const verifyToken = (token) => {
   return jsonwebtoken.verify(token, JWT.SECRET);
 };
 
-module.exports = { generateToen, verifyToken };
+module.exports = { generateToken, verifyToken };
