9704ab8ee518bb485f8a33c42702eb8e2d9e0fb2
Josué Yovani <josue.yovani@outlook.com>
2024-02-19 19:02:41 -0600

    Add - Functions encrypt

--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema(
@@ -6,11 +7,32 @@ const userSchema = new Schema(
     name: { type: String, trim: true },
     email: { type: String, trim: true, required: true, unique: true },
     password: { type: String, trim: true, required: true },
-    phoneNumber: { type: Number, trim: true },
+    phoneNumber: { type: Number },
     avatar: { type: String },
-    eventID: { type: mongoose.Schema.ObjectId, ref: "Event", required: true },
+    eventID: { type: mongoose.Schema.ObjectId, ref: "Event" },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    statics: {
+      encrypPassword: async (password) => {
+        try {
+          const salt = await bcrypt.genSalt(15);
+          const hashedPass = await bcrypt.hash(password, salt);
+          return hashedPass;
+        } catch (err) {
+          throw new Error(`Error: ${err.msg}`);
+        }
+      },
+      comparePassword: async (password, hash) => {
+        try {
+          const match = await bcrypt.compare(password, hash);
+          return match;
+        } catch (err) {
+          throw new Error(`Error: ${err.msg}`);
+        }
+      },
+    },
+  }
 );
 
 module.exports = model("User", userSchema);
