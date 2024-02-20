65275f7022e2d396b45e63bce3f49d3d4a0286d1
Josué Yovani <josue.yovani@outlook.com>
2024-02-19 20:42:37 -0600

    Add - Create index Routes

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+const pathRouter = `${__dirname}`;
+
+const removeExtension = (fileName) => {
+  return fileName.split(".").shift();
+};
+
+fs.readdirSync(pathRouter).filter((file) => {
+  const fileWithOutExt = removeExtension(file);
+  const skip = ["index"].includes(fileWithOutExt);
+  if (!skip) {
+    router.use(`/api/v1/${fileWithOutExt}`, require(`./${fileWithOutExt}`));
+  }
+});
+
+router.get("*", (req, res) => {
+  res
+    .status(404)
+    .send({ ok: false, msg: "Page not found", error: "Not found" });
+});
+
+module.exports = router;
