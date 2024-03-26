bd38cb6405b8876608986e0f3e12dba92c05ea5c
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-03-25 22:07:47 -0600

    undoing changes, I was working on dev instead of branch

--- src/lib/welcomeMail.js
+++ src/lib/welcomeMail.js
@@ -1,12 +1,12 @@
 const { sendMail } = require("../controllers/mail");
 
-const welcome = (mail, otp) => {
+const welcome = (mail) => {
   return sendMail({
     to: mail,
     from: "aceciliaramos@gmail.com",
     subject: "Verifica tu cuenta en Pata de Perro",
     test: "Has click en el enlace para verificar tu cuenta",
-    html: "<h1>VERIFICA TU CORREO ELECTRÓNICO</h1><br><div></div><br><button>Verificar</button>",
+    html: "<div>Haz clic en el siguiente botón para verificar tu dirección de correo electrónico y proteger tu privacidad.</div><br><button>Verificar</button>",
   });
 };
 
