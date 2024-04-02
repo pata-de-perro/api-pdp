f4dcb10f5b818b639a30642a5d014d02d27a6fc8
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-04-02 00:28:15 -0600

    checking sendgrid

---
+++ src/middlewares/isValid.js
@@ -0,0 +1,19 @@
+/** Imports **/
+const User = require("../models/users");
+
+const isValid = async (req, res, next) => {
+
+    const { isActive } = req.headers;
+  
+    try {
+     
+      }
+    
+  
+    //   next();
+    } catch (err) {
+      
+  };
+  
+  module.exports = { isValid };
+  
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { authUser } = require("../controllers/auth");
-const { captcha } = require("../middlewares/captcha")
 
 router.post("/sign-in", authUser);
 
