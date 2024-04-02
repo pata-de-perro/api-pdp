88b8909b40e3a5fab532083977bce8f246a11e67
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-04-02 16:22:51 -0600

    everything working

--- src/middlewares/isValid.js
+++ src/middlewares/isValid.js
@@ -6,15 +6,21 @@ const validateEmail = async (req, res, next) => {
   const user = await User.findOne({ emailToken: tokenProvided })
   try{
       if(!user){
-          res.status(404).json({ success: false, msg: "Token no valid" })
+          res
+          .status(404)
+          .json({ success: false, msg: "Token no valid" })
           return res.redirect('/')
       }
       user.emailToken = null;
       user.isActive = true;
       await user.save();
-      return res.redirect(`http://localhost:3000/account/login`)
+      return res
+      .status(201)
+      .send({ success: true, msg: "User verified!" });
   }   catch(error){
-      console.log(error)
+    return res
+      .status(err.status || 500)
+      .send({ success: false, msg: err.message || "Unknown" });
   }
 };
   
