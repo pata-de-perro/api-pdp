644a09db8c400648a7f7c1b45b42c15e0d4f49ff
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-04-02 00:45:03 -0600

    changing default email for from

--- src/lib/welcomeMail.js
+++ src/lib/welcomeMail.js
@@ -3,7 +3,7 @@ const { sendMail } = require("../controllers/mail");
 const welcome = (mail) => {
   return sendMail({
     to: mail,
-    from: "aceciliaramos@gmail.com",
+    from: "noreply@pata-de-perro.com",
     subject: "Verifica tu cuenta en Pata de Perro",
     test: "Has click en el enlace para verificar tu cuenta",
     html: "<div>Haz clic en el siguiente botón para verificar tu dirección de correo electrónico y proteger tu privacidad.</div><br><button>Verificar</button>",
