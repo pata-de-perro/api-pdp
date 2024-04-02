86e388d0cf0550f797c958ba149f1852e9e83c40
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-04-02 16:40:17 -0600

    rebase, everything working

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -64,23 +64,6 @@ const updateEventsById = async (req, res) => {
     return res
       .status(err.status || 500)
       .send({ success: false, msg: err.message || "Unknown" });
-  };
-
-};
-const addFriends = async (req, res) => {
-  const { id } = req.params;
-  const addFriend = req.body;
-
-  try {
-    const event = await Event.findByIdAndUpdate(id, addFriend, { new: true})
-    if (!event) {
-      return res.status(404).send({ success: false, msg: "Event not found"});
-    }
-    return res.status(200).send({ success: true, data: event});
-  } catch (err) {
-    return res
-    .status(err.status || 500)
-    .send({ success: false, msg: err.message || "Unknown"});
   }
 };
 const addFriends = async (req, res) => {
