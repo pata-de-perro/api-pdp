0e96ce9e109898fca8f285c1379dba1ab6717065
ceciliaramoscavazos <aceciliaramos@gmail.com>
2024-04-02 16:03:33 -0600

    isActive validation completed. Pending work: message errors on dom and redirect agter isValid middleware to login page

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,7 +10,7 @@ const { auth } = require("../middlewares/auth");
 const { captcha } = require("../middlewares/captcha")
 
 router.post("/", [captcha], createNewUser);
-router.get("/profile/:id", [auth], getUserProfile);
+router.get("/profile/:id", [ auth], getUserProfile);
 router.put("/profile-update/:id", [auth], updateUserProfile);
 
 module.exports = router;
