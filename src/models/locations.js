6fa9fd33346e8a1074bae71d159ee2fe9c4cfbf6
Josué Yovani <josue.yovani@outlook.com>
2024-04-03 00:22:54 -0600

    Fix - Model locations

--- src/models/locations.js
+++ src/models/locations.js
@@ -6,7 +6,7 @@ const locationSchema = new Schema(
     placeIdGoogle: { type: String, required: true },
     name: { type: String, trim: true, required: true },
     type: { type: String, trim: true, required: true },
-    vicinity: { typè: String, trim: true },
+    vicinity: { type: String, trim: true },
     simpleAddress: { type: String, trim: true, required: true },
     coords: { type: [Number], required: true },
   },
